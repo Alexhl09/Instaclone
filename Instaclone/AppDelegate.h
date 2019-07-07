68f502d01e6c2a91d8385d79ad4c7c7d37cdd13b
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-07-06 21:03:33 -0700

    Initial Commit

---
+++ Instaclone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instaclone
+//
+//  Created by alexhl09 on 7/6/19.
+//  Copyright © 2019 alexhl09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
